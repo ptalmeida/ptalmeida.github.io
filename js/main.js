ba9fe413e14abfbc40565a57ec92ec5223684a04
Pedro Almeida <pedro_98_almeida@hotmail.com>
2019-07-14 16:00:44 +0100

    Improve nav

--- js/main.js
+++ js/main.js
@@ -9,6 +9,11 @@
     $(this).toggleClass("active");
   });
 
+  $(".js-scroll-trigger").click(function(e) {
+    $("#sidebar-wrapper").removeClass("active");
+  });
+
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
